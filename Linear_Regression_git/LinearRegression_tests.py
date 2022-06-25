5d8425470caafdf8ce466f8fbef326fae2f313f3
pspriyankit <pspriyankit2@gmail.com>
2022-06-26 02:04:13 +0530

    commit

--- Linear_Regression_git/LinearRegression_tests.py
+++ Linear_Regression_git/LinearRegression_tests.py
@@ -32,7 +32,7 @@ fig = plt.figure(figsize=(4, 2))
 m1 = plt.scatter(X_train, y_train, color=cmap(0.8), s=5)
 m2 = plt.scatter(X_test, y_test, color=cmap(0.5), s=10)
 plt.plot(X, y_pred_line, color="black", linewidth=2, label="Prediction")
-plt.show()
+#plt.show()
 
 
 
